1832546ed3a8cefb518f937d01ca9b41ad541e91
Quân Huỳnh <huynhquandn1999@gmail.com>
2020-09-30 15:34:58 +0700

    State and Events

--- App.js
+++ App.js
@@ -1,18 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
-	return (
-		<View style={{ padding: 50 }}>
-			<View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', width: 100, height: 100 }}>
+	const [character, setCharacter] = useState('')
+	const [anime, setAnime] = useState([])
+	const nameInputed = (enteredName) => {
+		setCharacter(enteredName);
+	}
 
-				<TextInput placeholder="Input name here" style={{ borderColor: 'black', borderWidth: 1, padding: 10 }} />
-				<Button title="ADD" />
+	const onPressName = () => {
+		setAnime(currentValue => [...currentValue, character]);
+	}
+	return (
+		<View style={styles.screen}>
+			<View style={styles.viewItems}>
+
+				<TextInput placeholder="Input name here" style={styles.inputContainer} onChangeText={nameInputed} />
+				<Button title="ADD" onPress={onPressName} />
 
 			</View>
 		</View>
 	);
 }
+
+const styles = StyleSheet.create({
+	screen: {
+		padding: 50
+	},
+	inputContainer: {
+		borderColor: 'black',
+		borderWidth: 1,
+		padding: 10
+	},
+	viewItems: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		width: 100,
+		height: 100
+	}
+
+})
 
 
